6cf03e4fb5de22f65f4eb43b7ab73be9c4711ebe
TENSHKUMAR-KKT-2004 <tenshkumarkkt@gmail.com>
2023-06-28 22:57:58 +0530

    Build the smart contract for this app & test the smart contract

--- contracts/ToDoList.sol
+++ contracts/ToDoList.sol
@@ -1,5 +1,24 @@
+//SDPX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
 contract ToDoList{
     uint public taskCount = 0;
+
+    // new structure fo my individual tasks
+    struct Task{
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public{
+        createTask("Welcome to ToDoChain App !");
+    }
+
+    function createTask(string memory _content) public{
+        taskCount++;
+        tasks[taskCount] = Task(taskCount,_content,false);
+    }
 }
